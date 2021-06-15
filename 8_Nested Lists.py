7dc5250ea36187073a8e86ab81da0eeb6924eb99
Sethuaswin <84675499+Sethuaswin@users.noreply.github.com>
2021-06-15 16:21:19 +0530

    Question title updated

--- 8_Nested Lists.py
+++ 8_Nested Lists.py
@@ -1,4 +1,4 @@
-# Question 8:
+# Question 8 - Nested Lists:
 # Given the names and grades for each student in a class N of students,
 # store them in a nested list and print the name(s) of any student(s) having the second lowest grade.
 #
